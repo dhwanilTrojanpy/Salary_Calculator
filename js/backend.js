3105bc7059115d25ff0d44726b67fd6719e935f6
Dhwanil Gandhi <60458476+dhwanilTrojanpy@users.noreply.github.com>
2023-10-27 14:04:28 -0400

    V2- more abstracted and more formatted

--- js/backend.js
+++ js/backend.js
@@ -32,46 +32,60 @@ function calculateIncomeTax(incometax,gender,dependant){
 }
 
 function printDetails(firstName,grossPay,bonus,allowances,incometax,empInsurance,cpp,genderValue,dependantValue){
+    // return `
+    //         Name of the Employee : ${firstName}
+    //         Gender of the Employee : ${genderValue}
+    //         Number of dependants : ${dependantValue}
+    //         Total Salary : ${( grossPay + bonus + allowances )}
+    //         On Hand salary : ${calculateOnHandSalary(grossPay,bonus,allowances,incometax,empInsurance,cpp,genderValue,dependantValue)}
+    //         Total Deduction : ${((( grossPay + bonus + allowances ) * calculateIncomeTax(incometax,genderValue,dependantValue)+empInsurance+cpp))/100}
+    // `
     return `
-            Name of the Employee : ${firstName}
-            Gender of the Employee : ${genderValue}
-            Number of dependants : ${dependantValue}
-            Total Salary : ${( grossPay + bonus + allowances )}
-            On Hand salary : ${calculateOnHandSalary(grossPay,bonus,allowances,incometax,empInsurance,cpp,genderValue,dependantValue)}
-            Total Deduction : ${((( grossPay + bonus + allowances ) * calculateIncomeTax(incometax,genderValue,dependantValue)+empInsurance+cpp))/100}
+    <div>
+        <h5> Name of the Employee : ${firstName}</h5>
+        <h5> Gender of the Employee : ${genderValue}</h5>
+        <h5> Number of dependants : ${dependantValue}</h5>
+        <h5> Total Salary : ${( grossPay + bonus + allowances )} </h5>
+        <h5>  Total Deduction : ${((( grossPay + bonus + allowances ) * calculateIncomeTax(incometax,genderValue,dependantValue)+empInsurance+cpp))/100}</h5> 
+        <h5> On Hand salary : ${calculateOnHandSalary(grossPay,bonus,allowances,incometax,empInsurance,cpp,genderValue,dependantValue)} </h5>  
+    </div>
     `
+}
+function getFormValues(){
+   let formValues = {
+
+    firstName : document.querySelector('#firstName').value,
+    grossPay : parseInt(document.querySelector('#grossPay').value),
+    bonus : parseInt(document.querySelector('#bonus').value),
+    allowances : parseInt(document.querySelector('#allowances').value),
+    incometax : parseInt(document.querySelector('#incometax').value),
+    empInsurance : parseInt(document.querySelector('#empInsurance').value),
+    cpp : parseInt(document.querySelector('#cpp').value),
+
+    genderValue : document.getElementsByName("gender"),
+    dependantValue : document.getElementsByName("dependants"),
+    getGenderValue(){
+        for (let i = 0; i < this.genderValue.length;i ++){
+            if(this.genderValue[i].checked){
+              return this.genderValue[i].value
+            }
+        }
+    },
+    getDependantValue : function(){
+        for (let i = 0; i < this.dependantValue.length;i ++){
+            if(this.dependantValue[i].checked){
+               return this.dependantValue[i].value
+            }
+        }
+    },
+   }
+    return formValues 
 }
 
 form.addEventListener("submit",function(e){
    e.preventDefault();
-   
- 
-   const firstName = document.querySelector('#firstName').value;
-   const grossPay = parseInt(document.querySelector('#grossPay').value);
-   
-   const bonus = parseInt(document.querySelector('#bonus').value);
-   const allowances = parseInt(document.querySelector('#allowances').value);
-   
-   const incometax = parseInt(document.querySelector('#incometax').value);
-   const empInsurance = parseInt(document.querySelector('#empInsurance').value);
-   const cpp = parseInt(document.querySelector('#cpp').value);
-   
-   let genderValue
-   const gender = document.getElementsByName("gender");
-   for (let i = 0; i < gender.length;i ++){
-        if(gender[i].checked){
-            genderValue = gender[i].value
-        }
-   }
-
-   let dependantValue
-   const dependant = document.getElementsByName("dependants");
-   for (let i = 0; i < dependant.length;i ++){
-    if(dependant[i].checked){
-        dependantValue = parseInt(dependant[i].value)
-        }
-    }
-    console.log("income", typeof incometax)
-     result.innerHTML = printDetails(firstName,grossPay,bonus,allowances,incometax,empInsurance,cpp,genderValue,dependantValue);
+    
+   const form = getFormValues()
+   result.innerHTML = printDetails(form.firstName,form.grossPay,form.bonus,form.allowances,form.incometax,form.empInsurance,form.cpp,form.getGenderValue(),form.getDependantValue());
 
 });
